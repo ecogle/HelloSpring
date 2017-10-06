591d8c461fd3432cfd60f875c1ec57193e835015
ecogle <ecogle@c3developing.com>
2017-10-06 12:45:18 -0500

    test me

--- src/pkgone/TestPackage.java
+++ src/pkgone/TestPackage.java
@@ -1,5 +1,12 @@
 package pkgone;
 
+import java.math.BigDecimal;
+
 public class TestPackage {
 
+	public static void main(String[] args) {
+		Employee emp = new Employee("Chad", "Ogle",44, new BigDecimal("123400.98"));
+		
+		
+	}
 }
