73c70ec03487ee590953356662fbacd972f465dc
ecogle <ecogle@c3developing.com>
2017-10-07 01:17:55 -0500

    this pbi is fixed

--- src/pkgone/Employee.java
+++ src/pkgone/Employee.java
@@ -35,15 +35,5 @@ public class Employee extends Person {
 		System.out.println("Employee eating");
 	}
 
-	@Override
-	public void sleep() {
-		// TODO Auto-generated method stub
-		System.out.println("Employee sleeping");
-	}
-
-	@Override
-	public void drive() {
-		// TODO Auto-generated method stub
-		System.out.println("Employee driving");
-	}
+	
 }
